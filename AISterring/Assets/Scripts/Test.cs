70fc068e36adb597d3f87201b9f2aa338810a2cd
Frederik Pedersen <frecodev@gmail.com>
2018-03-14 15:22:42 +0100

    Array init

--- AISterring/Assets/Scripts/Test.cs
+++ AISterring/Assets/Scripts/Test.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using IronPython.Hosting;
 
 public class Test : MonoBehaviour {
 
+    public GameObject defaultTile;
+
+    GameObject[,] tiles = new GameObject[32,32];
+    int mapHeight = 32;
+    int mapWidth = 32;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //Initialize tilemap
+        for(int i = 0; i < mapWidth; i++)
+        {
+            for(int j = 0; j < mapHeight; j++)
+            {
+                tiles[i,j] = Instantiate(defaultTile, new Vector3(i,0,j), Quaternion.identity);
+            }
+        }              
+    }
 	
 	// Update is called once per frame
 	void Update () {
