260bd1d0977cd7201d5cd6563d1053cc734b4d88
Frederik Pedersen <frecodev@gmail.com>
2018-03-20 01:15:52 +0100

    Added travel-cost and textures to tile init
    Access travel-cost value from PathFinding script using:
    tiles[i, j].GetComponent<TileData>().weightValue;

--- AISterring/Assets/Scripts/PathFinding.cs
+++ AISterring/Assets/Scripts/PathFinding.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PathFinding : MonoBehaviour {
 
+    List<GameObject> open = new List<GameObject>(); //nodes that are getting evaluated
+    List<GameObject> closed = new List<GameObject>(); //nodes that have already been evaluated
+
 	// Use this for initialization
 	void Start () {
 		
