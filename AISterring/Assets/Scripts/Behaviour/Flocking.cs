0045f04798f95523a12302f3c14afead892ff15f
Jannik15 <jneerd15@student.aau.dk>
2018-03-26 16:05:32 +0200

    har du hørt om scripts?
    kun alignment OMEGALUL

---
+++ AISterring/Assets/Scripts/Behaviour/Flocking.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seperation : MonoBehaviour {
+
+    private Vector3 velocity = new Vector3(0,0,0);
+    private Vector3 alignmentVector;
+    private float dist;
+    private int neighbourCount = 0;
+    public GameObject[] agents = new GameObject[10];
+
+    public Vector3 ComputeAlignment()
+    {
+        foreach (GameObject agent in agents)
+        {
+            if (agent != this.gameObject)
+            {
+                dist = transform.position.x + transform.position.z - agent.transform.position.x - agent.transform.position.z;
+                if (dist < 0)
+                {
+                    dist *= -1;
+                }
+
+                if (dist < 300)
+                {
+                    alignmentVector.x += agent.transform.position.x;
+                    alignmentVector.z += agent.transform.position.z;
+                    neighbourCount++;
+                }
+            }
+        }
+
+        if (neighbourCount == 0)
+        {
+            return alignmentVector;
+        }
+        else
+        {
+            alignmentVector.x /= neighbourCount;
+            alignmentVector.z /= neighbourCount;
+            alignmentVector = alignmentVector.normalized;
+            alignmentVector *= Time.deltaTime;
+            return alignmentVector;
+        }
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().velocity += ComputeAlignment();
+    }
+}
