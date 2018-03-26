134cfb139ffb6cad81ea54fd199d97d979fab015
Jannik15 <jneerd15@student.aau.dk>
2018-03-26 16:45:12 +0200

    xd

--- AISterring/Assets/Scripts/Behaviour/Flocking.cs
+++ AISterring/Assets/Scripts/Behaviour/Flocking.cs
@@ -2,30 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seperation : MonoBehaviour {
+public class Flocking : MonoBehaviour {
 
-    private Vector3 velocity = new Vector3(0,0,0);
-    private Vector3 alignmentVector;
+    private Vector3 alignmentVector = new Vector3(0,0,0);
     private float dist;
     private int neighbourCount = 0;
-    public GameObject[] agents = new GameObject[10];
 
-    public Vector3 ComputeAlignment()
+    private Vector3 Alignment()
     {
-        foreach (GameObject agent in agents)
+        foreach (GameObject agent in GameObject.Find("AgentArray").GetComponent<AgentArrayScript>().array)
         {
             if (agent != this.gameObject)
             {
                 dist = transform.position.x + transform.position.z - agent.transform.position.x - agent.transform.position.z;
-                if (dist < 0)
+                if ((agent.transform.position - transform.position).magnitude < 100)
                 {
-                    dist *= -1;
-                }
-
-                if (dist < 300)
-                {
-                    alignmentVector.x += agent.transform.position.x;
-                    alignmentVector.z += agent.transform.position.z;
+                    alignmentVector.x += agent.GetComponent<Rigidbody>().velocity.x;
+                    alignmentVector.z += agent.GetComponent<Rigidbody>().position.z;
                     neighbourCount++;
                 }
             }
@@ -45,8 +38,72 @@ public class Seperation : MonoBehaviour {
         }
     }
 
+    private Vector3 Cohesion()
+    {
+        foreach (GameObject agent in GameObject.Find("AgentArray").GetComponent<AgentArrayScript>().array)
+        {
+            if (agent != this.gameObject)
+            {
+                dist = transform.position.x + transform.position.z - agent.transform.position.x - agent.transform.position.z;
+                if ((agent.transform.position - transform.position).magnitude < 100)
+                {
+                    alignmentVector.x += agent.transform.position.x;
+                    alignmentVector.z += agent.transform.position.z;
+                    neighbourCount++;
+                }
+            }
+        }
+
+        if (neighbourCount == 0)
+        {
+            return alignmentVector;
+        }
+        else
+        {
+            alignmentVector.x /= neighbourCount;
+            alignmentVector.z /= neighbourCount;
+            alignmentVector = new Vector3(alignmentVector.x - transform.position.x, alignmentVector.z - transform.position.z);
+            alignmentVector = alignmentVector.normalized;
+            alignmentVector *= Time.deltaTime;
+            return alignmentVector;
+        }
+    }
+
+    private Vector3 Seperation()
+    {
+        foreach (GameObject agent in GameObject.Find("AgentArray").GetComponent<AgentArrayScript>().array)
+        {
+            if (agent != this.gameObject)
+            {
+                dist = transform.position.x + transform.position.z - agent.transform.position.x - agent.transform.position.z;
+
+                if ((agent.transform.position - transform.position).magnitude < 50)
+                {
+                    alignmentVector.x += agent.transform.position.x - transform.position.x;
+                    alignmentVector.z += agent.transform.position.z - transform.position.z;
+                    neighbourCount++;
+                }
+            }
+        }
+
+        if (neighbourCount == 0)
+        {
+            return alignmentVector;
+        }
+        else
+        {
+            alignmentVector.x /= neighbourCount;
+            alignmentVector.z /= neighbourCount;
+            alignmentVector.x *= -1;
+            alignmentVector.z *= -1;
+            alignmentVector = alignmentVector.normalized;
+            alignmentVector *= Time.deltaTime;
+            return alignmentVector;
+        }
+    }
+
     private void Update()
     {
-        GetComponent<Rigidbody>().velocity += ComputeAlignment();
+        GetComponent<Rigidbody>().velocity += Alignment();
     }
 }
