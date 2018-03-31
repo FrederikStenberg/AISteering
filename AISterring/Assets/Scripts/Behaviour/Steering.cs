c8f1de15908c333400507289dc5cd222655e44ff
Frederik Pedersen <frecodev@gmail.com>
2018-03-31 14:30:51 +0200

    kk

--- AISterring/Assets/Scripts/Behaviour/Steering.cs
+++ AISterring/Assets/Scripts/Behaviour/Steering.cs
@@ -9,17 +9,35 @@ public class Steering : MonoBehaviour {
     public float maxForce;
     public float approachRadius;
 
+
     float dist;
     Vector3 velocity;
 
+    Vector3 ahead;
+    Vector3 ahead2;
+    public Vector3 maxSeeAhead;
+    public Vector3 maxAvoidForce;
+    public LayerMask obstacles;
+
     private void Awake()
     {
+        //For Seek
         velocity = GetComponent<Rigidbody>().velocity;
+ 
     }
 
     private void Update()
     {
         transform.position += Seek(leader);
+        avoidObstacle(leader);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "wall")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
     }
 
     Vector3 Seek(GameObject _leader)
@@ -41,4 +59,52 @@ public class Steering : MonoBehaviour {
         }
         return steer * Time.deltaTime;
     }
+
+    void avoidObstacle(GameObject _leader)
+    {
+        Vector3 target = _leader.transform.position - transform.position;
+
+        target = target.normalized;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 5f, obstacles))
+        {
+            if(hit.transform != transform)
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+                target += hit.normal * 400f;
+            }
+        }
+
+        Vector3 leftR = transform.position;
+        Vector3 rightR = transform.position;
+
+        leftR.x -= 2;
+        rightR.x += 2;
+
+        if(Physics.Raycast(leftR, transform.forward, out hit, 5f, obstacles))
+        {
+            if(hit.transform != transform)
+            {
+                Debug.DrawLine(leftR, hit.point, Color.red);
+                target += hit.normal * 400f;
+            }
+        }
+
+        if(Physics.Raycast(rightR, transform.forward, out hit, 5f, obstacles))
+        {
+            if(hit.transform != transform)
+            {
+                Debug.DrawLine(rightR, hit.point, Color.red);
+                target += hit.normal * 400f;
+            }           
+        }
+
+        Quaternion torotation = Quaternion.LookRotation(target);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, torotation, Time.deltaTime * 10f);
+
+        transform.position += transform.forward * 1f * Time.deltaTime;
+    }    
 }
